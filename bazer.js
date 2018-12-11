ff5a7b61d38700b54d301d37a6e7549ca6ee0c9f
jwlrai <jwolrai@gmail.com>
2018-12-11 15:00:39 -0800

    optimize

--- bazer.js
+++ bazer.js
@@ -10,9 +10,10 @@ var FollowBeizer = function(options){
     this.final_font          = options.final_font        || 20;
     this.spaceSize           = options.spaceSize         || 12;// sizes of space character
     this.charSpace           = options.charSpace         || 0;
+    this.font_grow           = options.font_grow         || 'grow'
 
     this.font_increment      = 0;
-    this.no_cordinates       = 100;
+    this.no_cordinates       = 150;
     this.start_point         = options.control_points[0];
     this.no_control_points   = options.control_points.length;
 
@@ -22,86 +23,86 @@ var FollowBeizer = function(options){
    
 }
 FollowBeizer.prototype.prepareTxt = function(){
-var txt = Array.from(this.text);   
-ele = document.getElementById(this.parent);
+    var txt = Array.from(this.text);   
+    ele = document.getElementById(this.parent);
 
-ele.innerHTML = '<div style="font-size:'+this.final_font+'px;position:absolute;visibility: hidden;">'+txt.join('</div><div  style="font-size:'+this.final_font+'px;position:absolute;visibility: hidden;">')+'</div>';
+    ele.innerHTML = '<div style="font-size:'+this.final_font+'px;position:absolute;visibility: hidden;">'+txt.join('</div><div  style="font-size:'+this.final_font+'px;position:absolute;visibility: hidden;">')+'</div>';
 
+    var start_font = 0;
+    if( this.font_grow=='grow'){
+        this.font_increment = (this.final_font-1)/this.no_cordinates;
+    }
+    else{
+        start_font = this.final_font;
+    }
+    
+   
+    var child = ele.children;
+    // getting the actual width of each characters
+    for(i=0;i < child.length;i++){
+        var wd = parseFloat(window.getComputedStyle(child[i], null).getPropertyValue('width'));
+        wd = (wd ===0)?this.spaceSize:wd;
+        this.charWidth.push(wd + this.charSpace );
+    }
+    console.log(start_font);
 
-this.font_increment = (this.final_font-1)/this.no_cordinates;
+    ele.innerHTML = '<div style="visibility: hidden;font-size:'+start_font+'px;position:absolute;" >'+txt.join('</div><div  style="visibility: hidden;font-size:'+start_font+'px;position:absolute;" >')+'</div>';
 
-var child = ele.children;
-// getting the actual width of each characters
-for(i=0;i < child.length;i++){
-    var wd = parseFloat(window.getComputedStyle(child[i], null).getPropertyValue('width'));
-    wd = (wd ===0)?this.spaceSize:wd;
-    this.charWidth.push(wd + this.charSpace );
-}
-
-ele.innerHTML = '<div style="font-size:0px;position:absolute;" >'+txt.join('</div><div  style="font-size:0px;position:absolute;" >')+'</div>';
-
-this.setControlPoints(ele);
-
-
+    this.setControlPoints(ele);
+   
 }
 FollowBeizer.prototype.setControlPoints = function(ele){
 
-var len = this.control_points.length;
-for(i=0; i < this.charWidth.length; i++){
-    var n_p = [];
-    for(j=0; j < len;j++){
-        if(j==len-1){
-            var ofWid = 0;
-            for(z=0;z<i;z++){
-                ofWid += this.charWidth[z];
+    var len = this.control_points.length;
+    for(i=0; i < this.charWidth.length; i++){
+        var n_p = [];
+        for(j=0; j < len;j++){
+            if(j==len-1){
+                var ofWid = 0;
+                for(z=0;z<i;z++){
+                    ofWid += this.charWidth[z];
+                }
+                
+                n_p.push([(this.control_points[j][0] +ofWid ),this.control_points[j][1]]);
+            }else{
+                n_p.push(this.control_points[j]);
             }
-            
-            n_p.push([(this.control_points[j][0] +ofWid ),this.control_points[j][1]]);
-        }else{
-            n_p.push(this.control_points[j]);
         }
+        this.real_con_point.push(n_p);
     }
-    this.real_con_point.push(n_p);
-}
- this.fetchCordinates(ele);
+    this.fetchCordinates(ele);
 }
 FollowBeizer.prototype.fetchBeizerPoint = function(cp,t,typ){
-var point = 0;
-for(var i=0; i < this.no_control_points; i++){
-    points  = Math.pow((1-t),(this.no_control_points-1-i) ) * cp[i][typ] * Math.pow(t,i);
-    if(i > 0 && i < (this.no_control_points-1) ){
-        points =  (this.no_control_points-1) * points;
+    var point = 0;
+    var n = this.no_control_points-1;
+    for(var i=0; i < this.no_control_points; i++){       
+        points = this.ponlynominals(t,n,i) *  cp[i][typ];
+        point = point+ points;
     }
-    point = point+ points;
-}
-return point;
+    return point;
 }
 FollowBeizer.prototype.fetchCordinates = function(ele){
 
-var seg = (1/this.no_cordinates).toFixed(2);
-for(j=0; j < this.real_con_point.length;j++){
-    var cp = this.real_con_point[j];
-    var n_p = [];
-    for(i=1; i <= this.no_cordinates; i++){
-        n_p.push([this.fetchBeizerPoint(cp,(seg*i),0),this.fetchBeizerPoint(cp,(seg*i),1)]);
+    var seg = (1/this.no_cordinates);
+    for(j=0; j < this.real_con_point.length;j++){
+        var cp = this.real_con_point[j];
+        var n_p = [];
+        for(i=1; i <= this.no_cordinates; i++){
+            n_p.push([this.fetchBeizerPoint(cp,(seg*i),0),this.fetchBeizerPoint(cp,(seg*i),1)]);
+        }
+        this.beizer_points.push(n_p);
     }
-    this.beizer_points.push(n_p);
-}
-this.moveDelay(ele.children,0,this.beizer_points.length,1);
+    this.moveDelay(ele.children,0,this.beizer_points.length,1);
 }
 
 // polynominals calculation
-FollowBeizer.ponlynominals = function (t,n,i){
-
+FollowBeizer.prototype.ponlynominals = function (t,n,i){
     var bio = Math.pow(t,i) * Math.pow((1-t),(n-i));
     var fac =  this.factor(n) / (this.factor(i) * this.factor(n-i)) ;
-
-    return bio* fac;
-    
+    return bio* fac;    
 }
 // factor calculation
-FollowBeizer.factor = function (number){
-
+FollowBeizer.prototype.factor = function (number){
     var fact = 1;
     if(number > 1){
         for(var i=number; i >1; i--){
@@ -114,109 +115,59 @@ FollowBeizer.factor = function (number){
 
 FollowBeizer.prototype.anim = function(ele,points,incre,aniPos){
 
-
-ele.style.left = points[incre][0]+'px';
-ele.style.top =  points[incre][1]+'px';   
-var fs = parseFloat(window.getComputedStyle(ele, null).getPropertyValue('font-size'))+this.font_increment;
-if( fs < this.final_font){
-    ele.style.fontSize =  fs+'px';
-}
-
-
-if(incre < points.length-1){
-    var ths = this;
-    setTimeout(function(){ 
-        ths.anim(ele,points,incre+1,aniPos);
-    }, this.aniDelay );
-}
-else if(aniPos == this.charWidth.length){
-    if(this.cb !==undefined){
-        this.cb();
+    ele.style.left = points[incre][0]+'px';
+    ele.style.top =  points[incre][1]+'px';   
+    if( this.font_grow=='grow'){
+        var fs = parseFloat(window.getComputedStyle(ele, null).getPropertyValue('font-size'))+this.font_increment;
+        if( fs < this.final_font){
+            ele.style.fontSize =  fs+'px';
+        }
+    }
+    else{
+        ele.style.visibility = 'visible';
+    }
+    if(incre < points.length-1){
+        var ths = this;
+        setTimeout(function(){ 
+            ths.anim(ele,points,incre+1,aniPos);
+        }, this.aniDelay );
+    }
+    else if(aniPos == this.charWidth.length){
+        if(this.cb !==undefined){
+            this.cb();
+        } 
     } 
-} 
 
 }
 FollowBeizer.prototype.moveDelay = function(ele,i,len,aniPos){   
-this.anim(ele[i],this.beizer_points[i],0,aniPos);
-if(i <= len){
-    var ths = this;
-    setTimeout(function(){ 
-        if(ths.beizer_points[i+1]!==undefined){
-            ths.moveDelay(ele,i+1,len,aniPos+1);
-        }
-    }, this.charDelay);
-}
+    this.anim(ele[i],this.beizer_points[i],0,aniPos);
+    if(i <= len){
+        var ths = this;
+        setTimeout(function(){ 
+            if(ths.beizer_points[i+1]!==undefined){
+                ths.moveDelay(ele,i+1,len,aniPos+1);
+            }
+        }, this.charDelay);
+    }
 }
 
 FollowBeizer.prototype.start = function(cb){
-this.cb = cb;
-this.prepareTxt(document.getElementById(this.id));  
+    this.cb = cb;
+    this.prepareTxt(document.getElementById(this.id));  
 }
 
 var obj = new FollowBeizer({
 parent          :'nav',
-text            :'-> Ujwal Rai',
-control_points  : [[350,350],[650,450],[550,300],[400,300],[400,200],[600,200],[600,100],[0,80]],
-end_text_align  :'',
-final_font      :70,
-charDelay:200,
-aniDelay :8
-});
-
-
-
-var para = new FollowBeizer({
-parent          :'title',
-text            :'Full Stack Developer | Focus on backend',
-control_points  : [[350,350],[850,450],[550,300],[400,300],[400,200],[600,200],[600,100],[0,240]],
+text            :'samsung',
+control_points  : [[0,0],[500,500],[1000,0]],
 end_text_align  :'',
 final_font      :20,
-// charSpace :1,
-charDelay:80,
-aniDelay :5
+charDelay:200,
+aniDelay :20,
+font_grow:'constant'
 });
 
-function mixColor(id,no,off,cb){
-// document.getElementById(id).children[no].style.color='#19a57f';
-document.getElementById(id).children[no].style.color='rgb(18, 109, 152)';
+obj.start(function(){
 
+});
 
-if(no < off){
-    setTimeout(function(){ 
-       
-        mixColor(id,no+1,off,cb);
-    },100);
-}
-else{
-    if(cb!==undefined){
-        cb();
-    }        
-}
-}
-
-obj.start(function(){para.start(function(){
-// mixColor('nav',0,7,function(){
-  
-// });
-});});
-setTimeout(function(){ 
-       
-document.querySelector('nav ul li:nth-child(3)').style.marginLeft = '0';
-
-
-setTimeout(function(){ 
-
-document.querySelector('nav ul li:nth-child(2)').style.marginLeft = '0';
-setTimeout(function(){ 
-    
-    
-    document.querySelector('nav ul li:nth-child(1)').style.marginLeft= '0';
-    document.querySelector('nav').style.marginLeft= '100px';
-    setTimeout(function(){
-        document.querySelector('nav').style.marginLeft= '0px';
-    },800);
-    
- },1400);
-
-},1400);
-},1400);
